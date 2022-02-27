1145f02fb4a612795f056fc87a27063c777fca2a
Asurazzz <2513667783@qq.com>
2022-02-27 15:10:22 +0800

    事务

---
+++ rocket07_rocket_simple_demo/src/main/java/com/ymj/demo4/ConsumerTransaction.java
@@ -0,0 +1,29 @@
+package com.ymj.demo4;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class ConsumerTransaction {
+    public static void main(String[] args) throws Exception{
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("transaction_consumer");
+        consumer.setNamesrvAddr("192.168.150.11:9876;192.168.150.12:9876");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe("topic_transaction", "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                msgs.forEach(msg -> System.out.println(msg));
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.in.read();
+
+    }
+}
